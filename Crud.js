f45cf56d26bfb2155e1c74c75aee1c1976140c07
Quaid Ahmed <inoxent5050@gmail.com>
2023-05-01 00:33:40 +0500

    implement the delete function

--- Crud.js
+++ Crud.js
@@ -2,3 +2,8 @@ console.log("hello dear this is crud applications so we ");
 console.log("commit from quaid ahmed");
 console.log("commit from my side  its fazi ");
 console.log("hey quaid ahmed");
+console.log("can we do listing in world and laravel project and can be work with different site ");
+
+for (let i = 34; i < 43; i++) {
+    console.log("hello wolld")
+}
